a897df151fd2ca18f6247608589e0631d5e3ec98
Hstarorg <hm910705@163.com>
2018-01-28 00:14:07 +0800

    Add jpush support

--- FoodyMouthApp/android/app/src/main/java/com/foodymouthapp/MainApplication.java
+++ FoodyMouthApp/android/app/src/main/java/com/foodymouthapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.foodymouthapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cn.jpush.reactnativejpush.JPushPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -15,6 +16,11 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  // (jPush)设置为 true 将不会弹出 toast
+  private boolean SHUTDOWN_TOAST = true;
+  // (jPush)设置为 true 将不会打印 log
+  private boolean SHUTDOWN_LOG = false;
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -24,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new JPushPackage(SHUTDOWN_TOAST, SHUTDOWN_LOG),
             new RNI18nPackage(),
             new SplashScreenReactPackage());
     }
